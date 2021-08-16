3ccdaabde7cac6acc2636ee3b445caa7501892d9
ViacheslavBezditniy <vbezditniy@gmail.com>
2021-08-16 13:11:04 +0300

    add assets

---
+++ Assets/Grenade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour {
+
+	public Transform explosionPrefab;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		ContactPoint contact = collision.contacts[0];
+
+		// Rotate the object so that the y-axis faces along the normal of the surface
+		Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+		Vector3 pos = contact.point;
+		Instantiate(explosionPrefab, pos, rot);
+		Destroy(gameObject);
+	}
+}

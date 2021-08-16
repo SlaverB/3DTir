994139f8db8f3b0134cba650fe2788f0947e023a
ViacheslavBezditniy <vbezditniy@gmail.com>
2021-08-16 18:02:16 +0300

    add TargetGenerator

---
+++ Assets/Scripts/TargetGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+
+    public int numObjects = 10;
+
+    void Start()
+    {
+        Vector3 center = transform.position;
+        for (int i = 0; i < numObjects; i++)
+        {
+            Vector3 pos = RandomCircle(center, 5.0f);
+            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
+            Instantiate(_target, pos, rot);
+        }
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * Random.Range(-90, 90);
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
+    }
+}
